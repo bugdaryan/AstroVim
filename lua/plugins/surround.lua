41ca56c7a129e8654e0655f2315309df841d153e
bugdaryan <bugdaryan@gmail.com>
2025-08-25 13:59:53 +0400

    Add surround plugin

---
+++ lua/plugins/surround.lua
@@ -0,0 +1,15 @@
+---@type LazySpec
+return {
+  "kylechui/nvim-surround",
+  version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+  event = "VeryLazy",
+  config = function()
+    require("nvim-surround").setup {
+      surrounds = {
+        ["("] = { add = { "(", ")" } },
+        ["["] = { add = { "[", "]" } },
+        ["{"] = { add = { "{", "}" } },
+      },
+    }
+  end,
+}
